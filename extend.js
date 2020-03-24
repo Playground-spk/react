53fb4c8e0d3e23097191012b8316ce6550edf2b3
spk-cpsamut <taresupakon@gmail.com>
2020-03-24 12:19:09 +0700

    mimi

---
+++ extend.js
@@ -0,0 +1,33 @@
+class Animal {
+  constructor(aname) {
+    this.name = aname;
+    this.kingdom = "Animalia";
+    this.alive = true;
+  }
+  roar() {
+    alert("meow");
+  }
+  sayHi() {
+    console.log("hi");
+  }
+}
+
+class Feral extends Animal {
+  constructor(aname) {
+    super(aname);
+    this.cute = "kawaii";
+    this.fang = true;
+  }
+  play() {
+    console.log("kill human");
+  }
+}
+
+let dog = new Animal("Bog");
+let cat = new Feral("Mimi");
+
+dog.kingdom; // ได้ Animalia
+cat.Kingdom; // ได้ Animalia
+
+dog.cute; // undefined
+cat.cute; // "kawaii"
